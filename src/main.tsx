04363bdf840a4c8fc0d2051d82689c3d07d8406b
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-09-03 00:02:19 -0600

    init files structure

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { YousacApp } from "./YousacApp";
+
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <YousacApp />
+  </React.StrictMode>
+);
