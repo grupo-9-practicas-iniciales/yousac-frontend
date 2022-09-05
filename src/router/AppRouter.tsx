cb170378e3d919d88da3e2933075849ddbb2f185
Damian <damianpeaf@gmail.com>
2022-09-04 19:15:23 -0600

    [ ADD ] Landing Page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import { LandingPage } from '../pages';
+
 export const AppRouter = () => {
+
+  // ! Validate the auth state
+
   return (
-    <div>AppRouter</div>
+    <Routes>
+      <Route path='/auth/login' element={<></>} />
+      <Route path='/*' element={<LandingPage />} />
+    </Routes>
   )
 }
