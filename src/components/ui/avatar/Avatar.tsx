94a50dcd784164303f5a99057f89adbb5073eccb
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-09-05 14:04:29 -0600

    [ ADD ] Creating Avatar component

--- src/components/ui/avatar/Avatar.tsx
+++ src/components/ui/avatar/Avatar.tsx
@@ -1,5 +1,54 @@
-export const Avatar = () => {
+import { Popover, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { CgProfile, CgLogOff } from "react-icons/cg";
+import { Link } from "react-router-dom";
+
+type Props = {
+  src: string;
+  alt: string;
+  isUserActive?: boolean;
+};
+
+export const Avatar = ({ src, alt, isUserActive = false }: Props) => {
   return (
-    <div>Avatar</div>
-  )
-}
+    <Popover as="div" className="relative inline-block text-left">
+      {({ open }) => (
+        <>
+          <Popover.Button>
+            <div className="relative">
+              <div
+                className={isUserActive ? "avatar-isUserActive" : "hidden"}
+              ></div>
+              <img className="h-10 w-10 rounded-full" src={src} alt={alt} />
+            </div>
+          </Popover.Button>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute right-0 mt-2 w-24 md:w-40 origin-top-right divide-y rounded-sm divide-gray-400 dark:divide-primary-dark-3/[0.1]  bg-white dark:bg-darkContrast shadow-lg dark:shadow-none ring-1 ring-black dark:ring-primary-dark-3/[0.1] ring-opacity-5  focus:outline-none  dark:text-white">
+              <div className="py-1">
+                <Link to="/profile" className="avatar-option-menu">
+                  <CgProfile className="mr-2" />
+                  Perfil
+                </Link>
+              </div>
+              <div className="py-1">
+                <Link to="/auth/login" className="avatar-option-menu">
+                  <CgLogOff className="mr-2" />
+                  Cerrar sesión
+                </Link>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
+  );
+};
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,29 +1,44 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { Waves1SVG, RocketSVG } from "../assets";
-import { DownArrowIcon } from '../assets/icons/DownArrowIcon';
-import { ToogleThemeButton } from '../components/theme/ToogleThemeButton';
+import { DownArrowIcon } from "../assets/icons/DownArrowIcon";
+import { ToogleThemeButton } from "../components/theme/ToogleThemeButton";
+import { Avatar } from "../components/ui/";
 
 export const LandingPage = () => {
-    return (
-        <main className="min-h-screen bg-light dark:bg-dark overflow-x-hidden transition-colors" >
-            <nav className="h-[10vh] flex items-center justify-around" >
-                <h1 className="text-logo font-bold text-[28px] lg:text-[36px]">YOUSAC</h1>
-                <Link className="text-black dark:text-white font-bold text-[12px] lg:text-[16px] hover:text-gray-100" to="/auth/login">
-                    Iniciar sesión
-                </Link>
-            </nav>
-            <section className="h-[70vh] flex flex-col gap-y-8 mt-10">
-                <RocketSVG className="w-full h-full " viewBox="0 0 326 239" />
-                <h2 className="text-black dark:text-white text-center text-[16px] lg:text-[22px]">Las mejores Recomendaciones</h2>
-                <div className="flex justify-center items-center">
-                    <DownArrowIcon className="text-primary-light-1 dark:text-primary-dark-1 text-2xl lg:text-3xl" />
-                </div>
-
-            </section>
-            <div className="h-[10vh] absolute bottom-0 right-0 w-full">
-                <Waves1SVG className="w-full" preserveAspectRatio="none" viewBox="0 0 300 100" />
-            </div>
-            <ToogleThemeButton fab={true} />
-        </main>
-    )
-}
+  return (
+    <main className="min-h-screen bg-light dark:bg-dark overflow-x-hidden transition-colors">
+      <nav className="h-[10vh] flex items-center justify-around">
+        <h1 className="text-logo font-bold text-[28px] lg:text-[36px]">
+          YOUSAC
+        </h1>
+        <Link
+          className="text-black dark:text-white font-bold text-[12px] lg:text-[16px] hover:text-gray-100"
+          to="/auth/login"
+        >
+          Iniciar sesión
+        </Link>
+        <Avatar
+          src="https://avatars.githubusercontent.com/u/81493003?v=4"
+          alt="User Photo"
+        />
+      </nav>
+      <section className="h-[70vh] flex flex-col gap-y-8 mt-10">
+        <RocketSVG className="w-full h-full " viewBox="0 0 326 239" />
+        <h2 className="text-black dark:text-white text-center text-[16px] lg:text-[22px]">
+          Las mejores Recomendaciones
+        </h2>
+        <div className="flex justify-center items-center">
+          <DownArrowIcon className="text-primary-light-1 dark:text-primary-dark-1 text-2xl lg:text-3xl" />
+        </div>
+      </section>
+      <div className="h-[10vh] absolute bottom-0 right-0 w-full">
+        <Waves1SVG
+          className="w-full"
+          preserveAspectRatio="none"
+          viewBox="0 0 300 100"
+        />
+      </div>
+      <ToogleThemeButton fab={true} />
+    </main>
+  );
+};
