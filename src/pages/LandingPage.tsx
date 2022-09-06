2e5df64c3021e17053d19907cd7c96323cd4ab4a
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-09-05 23:32:53 -0600

    [ ADD ] Create Button component for app

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { Waves1SVG, RocketSVG } from "../assets";
 import { DownArrowIcon } from "../assets/icons/DownArrowIcon";
 import { ToogleThemeButton } from "../components/theme/ToogleThemeButton";
-import { Avatar } from "../components/ui";
+import { Avatar, Button } from "../components/ui";
 import { User } from "../context/auth";
 
 const testInitialUser2: User = {
@@ -12,6 +12,10 @@ const testInitialUser2: User = {
   idStudent: "12321",
   idUser: "asdada",
   token: "",
+};
+
+const testFunction = () => {
+  console.log("test");
 };
 
 export const LandingPage = () => {
@@ -45,6 +49,12 @@ export const LandingPage = () => {
           viewBox="0 0 300 100"
         />
       </div>
+      <Button
+        text="Hola"
+        variant="secondary"
+        className="mx-3 w-32"
+        onClick={testFunction}
+      />
       <ToogleThemeButton fab={true} />
     </main>
   );
