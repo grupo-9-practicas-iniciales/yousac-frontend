998dbdd9c3fa0596a8474fea4b3c570f9bf98592
Josmalopez77 <100547471+Josmalopez77@users.noreply.github.com>
2022-09-10 22:43:49 -0600

    Creacion de los componentes, title, subtitle, paragraphs

---
+++ src/components/ui/textComponent/Paragraphs.tsx
@@ -0,0 +1,11 @@
+export const Paragraphs = () => {
+  return (
+    <div className= 'bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl'>
+        <p className="text-[12px] sm:text-[14px] lg:text-[18px] text-slate-500 dark:text-slate-400 mt-2 text-justify" >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum aliquam, maiores ipsum
+            eius alias aliquid quidem repellat odit rerum minima voluptate! Nobis vel temporibus
+            sit itaque quos consequuntur doloribus accusantium?
+    </p>
+        </div>
+  )
+}
---
+++ src/components/ui/textComponent/Subtitle.tsx
@@ -0,0 +1,7 @@
+export const Subtitle = () => {
+  return (
+    <div className= 'bg-white dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl'>
+        <h2 className="text-[14px] sm:text-[16px] lg:text-[20px] text-slate-500 dark:text-slate-400 mt-2 text-left" >Subtitle</h2>
+        </div>
+  )
+}
