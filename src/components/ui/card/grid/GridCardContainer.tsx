020c4f3664352386d48f91f8bafe65bc86691ba6
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-09-11 22:09:25 -0600

    [ ADD ] Card Course created

---
+++ src/components/ui/card/grid/GridCardContainer.tsx
@@ -0,0 +1,12 @@
+type GridCardContainerProps = {
+  children: React.ReactNode[];
+};
+export const GridCardContainer = ({ children }: GridCardContainerProps) => {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-4 md:gap-6 w-5/6">
+        {children}
+      </div>
+    </>
+  );
+};
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,8 +1,6 @@
 import { Navigate } from "react-router-dom";
 
-import { BsFillPencilFill } from "react-icons/bs";
-
-import { ScrollToTop, Navbar, CardUser, Button } from "../components";
+import { ScrollToTop, Navbar, CardUser, Card ,GridCardContainer, CardCourse } from "../components";
 import { useContentStore } from "../hooks";
 
 export const ProfilePage = () => {
@@ -17,11 +15,20 @@ export const ProfilePage = () => {
       <ScrollToTop />
       <Navbar />
       <div
-        className="flex flex-col items-center mt-5 md:mt-7 p-10 md:p-16 lg:p-0 justify-center mx-auto min-h-screen w-full
+        className="flex flex-col justify-start items-center mt-5 md:mt-7 p-10 md:p-16 lg:p-0 min-h-screen w-full
         animate__animated animate__fadeIn"
       >
         <div className="max-w-2xl w-full">
           <CardUser user={selectedUser} behavior="default" />
+          <GridCardContainer>
+            {
+              selectedUser.courses.map((course) => (
+                <Card key={course.idCourse}>
+                  <CardCourse course={course} />
+                </Card>
+            }
+
+          </GridCardContainer>
         </div>
       </div>
     </div>
