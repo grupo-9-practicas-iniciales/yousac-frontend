21b7904534f2514fcf593d96f708a99a461a8d8e
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-09-11 19:08:08 -0600

    [ UPDATE ] Add new variant in buttons

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,10 @@
-import { useContentStore } from "../hooks";
 import { Navigate } from "react-router-dom";
-import { ScrollToTop, Navbar, CardUser } from "../components";
+
+import { BsFillPencilFill } from "react-icons/bs";
+
+import { ScrollToTop, Navbar, CardUser, Button } from "../components";
+import { useContentStore } from "../hooks";
+
 export const ProfilePage = () => {
   const { selectedUser } = useContentStore();
 
@@ -13,7 +17,7 @@ export const ProfilePage = () => {
       <ScrollToTop />
       <Navbar />
       <div
-        className="flex mt-5 md:mt-7 p-10 md:p-16 lg:p-0 justify-center mx-auto min-h-screen w-full
+        className="flex flex-col items-center mt-5 md:mt-7 p-10 md:p-16 lg:p-0 justify-center mx-auto min-h-screen w-full
         animate__animated animate__fadeIn"
       >
         <div className="max-w-2xl w-full">
